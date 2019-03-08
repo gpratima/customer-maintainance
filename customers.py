c0d3c5d866cad7a44583b4c117e05f1ee341a7ba
gpratima <48248752+gpratima@users.noreply.github.com>
2019-03-08 17:40:31 +0530

    Create customers.py

---
+++ customers.py
@@ -0,0 +1,33 @@
+import json
+
+class Customers:
+
+    def __init__(self):
+        self.customer = []
+
+    def add_customer(self, cust_name, cust_type, address, state, phone):
+        new_customer = {}
+        new_customer["Customer Name: "] = cust_name
+        new_customer["Customer Type: "] = cust_type
+        new_customer["Addres: "]= address
+        new_customer["State: "]= state
+        new_customer["Phone: "]= phone
+        self.customer.append(new_customer)
+        print("Customet: {0}".format(new_customer))
+        return json.dumps(new_customer)
+
+    def del_customer(self, phone):
+        found = False
+        for idx, book in enumerate(self.customer):
+            if book["Phone"] == phone:
+                index = idx
+                found = True
+                del self.customer[idx]
+        print("customer: {0}".format(json.dumps(self.customer)))
+        return found
+
+    def get_all_customer(self):
+        return self.customer
+
+    def json_list(self):
+        return json.dumps(self.customer)
