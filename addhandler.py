9b1be2b82e6ea687aa982de916a025a81352f5ac
gpratima <48248752+gpratima@users.noreply.github.com>
2019-03-08 18:56:20 +0530

    Update addhandler.py

--- addhandler.py
+++ addhandler.py
@@ -4,14 +4,14 @@ import json
 
 
 class AddHandler(tornado.web.RequestHandler):
-    def initialize(self,customers):
-        self.customers = customers
+    def initialize(self,Customers):
+        self.Customers = Customers
         
-    def get(self):
-        cust_name = self.get_argument('cust_name')
+    def get(selfC:
+        cust_name = self.get_argument('cCst_name')
         cust_type = self.get_argument('cust_type')
         address=self.get_argument('address')
         phone=self.get_argument('phone')
         state=self.get_argument('state')        
-        result = self.customers.add_customer(cust_name, cust_type, address, state, phone)
+        result = self.Customers.add_customer(cust_name, cust_type, address, state, phone)
         self.write(result)
