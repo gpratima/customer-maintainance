08bfd75e91e059dc618361351af84694d51e16c5
gpratima <48248752+gpratima@users.noreply.github.com>
2019-03-08 19:00:05 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import tornado.ioloop
 import tornado.web
-from customers import CUSTOMERS
+from customers import CUSTOMER
 from addhandler import AddHandler
 from delhandler import DelHandler
 from gethandler import GetHandler
