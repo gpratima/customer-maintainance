f3495753799fe3a22c0357f23be13114c7f954cd
gpratima <48248752+gpratima@users.noreply.github.com>
2019-03-09 18:08:47 +0530

    Update Cust_DB.py

--- Cust_DB.py
+++ Cust_DB.py
@@ -8,8 +8,8 @@ class CUSTOMER:
     def add_customer(self, cust_name, cust_type, state):
         new_customer = {}
         new_customer["cust_name"] = cust_name
-        new_customer["Customer Type: "] = cust_type
-        new_customer["State: "]= state
+        new_customer["cust_type"] = cust_type
+        new_customer["state"]= state
         self.Customers.append(new_customer)
         print("CUSTOMER: {0}".format(new_customer))
         return json.dumps(new_customer)
